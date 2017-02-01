ea08df873331093620fcde00cd8906883bae05fe
jakim54 <james.kim@huffingtonpost.com>
2017-02-01 12:32:38 -0500

    completed bankvault

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -7,7 +7,7 @@
 //
 
 
-class BankVault {
+class BankVault: ProvideAccess {
     
     let name: String
     let address: String
@@ -18,9 +18,24 @@ class BankVault {
         self.address = address
     }
     
+    func allowEntryWithPassword(password: [Int]) -> Bool {
+        if password.count > 10 || password.count < 0 {
+            return false
+        } else {
+            let numOfPw = password.count
+            let isItEven = password.filter { $0 % 2 == 0 }.count
+            return isItEven == numOfPw ? true : false
+        }
+    }
+    
 }
 
-
-
+protocol ProvideAccess {
+//     no body for functions in protocol functions. I guess because it's to be implemented with their own variations within classes and extensions following it?
+//    static func someTypeMethod()
+//    func random() -> Double
+    
+    func allowEntryWithPassword(password: [Int]) -> Bool
+}
 
 
