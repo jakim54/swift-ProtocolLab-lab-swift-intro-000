6256c37b4571caa101ff645e8a27e4726e1c9ce2
jakim54 <james.kim@huffingtonpost.com>
2017-02-02 17:05:42 -0500

    solved, overthought various solutions, but ended up settling for what's pushed to master

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -22,27 +22,17 @@ class BankVault: ProvideAccess {
         if password.count > 10 || password.isEmpty {
             return false
         } else {
-            // used force unwrapped optional, but only because it passed isEmpty. Otherwise, IUO!
             if password.first! % 2 != 0 {
                 return false
             } else {
-                // use enumerated to ensure that every other number is even from the first. So index 2, 4, 6 should be even numbers. Not the entire thing. I think you can add +1 to filter condition. $0 + 1?
-                let numOfPw = password.count
-                let isItEven = password.filter { $0 % 2 == 0 }.count
-                
-                return isItEven == numOfPw ? true : false
+                let confirmation = password.contains { $0 % 2 == 0 }
+                return confirmation
             }
         }
-    }
+    } // end of function
     
 }
 
 protocol ProvideAccess {
-//     no body for functions in protocol functions. I guess because it's to be implemented with their own variations within classes and extensions following it?
-//    static func someTypeMethod()
-//    func random() -> Double
-    
     func allowEntryWithPassword(password: [Int]) -> Bool
 }
-
-
